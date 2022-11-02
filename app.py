cc1054a734b4c3d117d2e757f89176a46176484e
hierox <dwcanillas@gmail.com>
2022-11-01 23:18:58 -0500

    add main functionality

--- app.py
+++ app.py
@@ -2,31 +2,74 @@
 import json
 import os
 import requests
+import datetime
 import time
 
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', methods=["GET"])
-def get_hello():
-    return ("Hello, Get", 200)
-
-@app.route("/", methods=["POST"])
-def post_hello():
-    return ("Hello, Post", 200)
-
-@app.route("/Scotch", methods=["GET"])
-def scotch_hello_get():
-    return ("Hello, Scotch Get", 200)
-
 @app.route("/Scotch", methods=["POST"])
-def scotch_hello_post():
-    return ("Hello, Scotch Post", 200)
+def scotch_message():
+    
+    # it probably would be better design to read this in from a file or database, but i'm lazy
+    scotch_list = []
+    scotch_list.append("Lagavulin 8 (Jar 1/A)")
+    scotch_list.append("Talisker Storm (Jar 2/B)")
+    scotch_list.append("Glenlivet 12 (Jar 3/C)")
+    scotch_list.append("Oban Little Bay (Jar 4/D)")
+    scotch_list.append("Monkey Shoulder Batch 27 (Jar 5/E)")
+    scotch_list.append("Tullibardine 12 (Jar 6/F)")
+    scotch_list.append("Aucentoshan 3 wood (Jar 7/G)")
+    scotch_list.append("Glenfiddich 12 (Jar 8/H)")
+    scotch_list.append("Ardbeg Corryvreckan (Jar 9/I)")
+    scotch_list.append("Ardbeg Uigeadail (Jar 10/J)")
+    scotch_list.append("Smokehead High Voltage (Jar 11/K)")
+    scotch_list.append("Glenmorangie 10 (Jar 12/L)")
+    scotch_list.append("Springbank 15 (Jar 13/M)")
+    scotch_list.append("Aberfeldy 12 (Jar 14/N)")
+    scotch_list.append("Auchentoshan Single 12 (Jar 15/O)")
+    scotch_list.append("Creag Isle 12 Single Malt (Jar 16/P)")
+    scotch_list.append("Macallan 12 Double Cask (Jar 17/Q)")
+    scotch_list.append("Kilchoman (Jar 18/R)")
+    scotch_list.append("Edradour 10 (Jar 19/S)")
+    scotch_list.append("Lagavulin 9 (Jar 20/T)")
+    scotch_list.append("Glen Fohdry 12 (Jar 21/U)")
+    scotch_list.append("Grangestone Bourbon Cask Finish (Jar 22/V)")
+    scotch_list.append("Jura 10 (Jar 23/W)")
+    scotch_list.append("Redbreast (Jar 24/X)")
 
-@app.route("/Trivia", methods=["GET"])
-def trivia_hello_get():
-    return ("Hello, Trivia Get", 200)
+    # get todays date
+    today = datetime.datetime.today()
+    if (today.year != 2022): # we don't want this running next year
+        return
+    # if (today.month != 12): # start in december
+    #    return
+    actual_day = today.day - 1 # the bot runs after midnight UTC
+    if (actual_day < 1):
+        return
+    if (actual_day > len(scotch_list)):
+        return
+
+    # python is 0 indexed
+    scotch = scotch_list[actual_day - 1]
+    
+    # send the message
+    return send_message("Today's Scotch is: " + scotch, os.getenv("SCOTCH_GROUP_API_KEY")) 
+
 
 @app.route("/Trivia", methods=["POST"])
 def trivia_hello_post():
-    return ("Hello, Trivia Post", 200)
+    return send_message("Like for trivia tonight at 8 at Black Sheep Pizza (North Loop)", os.getenv("TRIVIA_GROUP_API_KEY"))
+
+
+def send_message(msg, api_key):
+    print("sending message: %s" % msg)
+
+    url = "https://api.groupme.com/v3/bots/post"
+
+    payload = {
+        "bot_id": api_key,
+        "text": msg
+    }
+
+    return requests.post(url, json=payload)
