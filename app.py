d4223e3e3b2305999c96ef7b2980157bd3865523
David Canillas <dwcanillas@gmail.com>
2025-11-30 22:43:48 -0600

    Updates for 2025

--- app.py
+++ app.py
@@ -17,39 +17,40 @@ def awaken_service():
 @app.route("/scotch", methods=["POST"])
 def scotch_message():
     
-    # it probably would be better design to read this in from a file or database, but i'm lazy
+    # it probably would be better design to read this in from a file or database
     scotch_list = []
-    scotch_list.append("1792 Full Proof Total Wine Barrel Pick (Jar 1/A)")
-    scotch_list.append("Clyde May's Special Reserve (Jar 2/B)")
-    scotch_list.append("Bunnahabhain 12 (Jar 3/C)")
-    scotch_list.append("New Richmond Rye (Jar 4/D)")
-    scotch_list.append("Port Charlotte 10 year (Jar 5/E)")
-    scotch_list.append("Glenmorangie 14 yr Quinta Ruban (Jar 6/F)")
-    scotch_list.append("Willet Straight Rye Whiskey (Jar 7/G)")
-    scotch_list.append("Tattersall Straight Wheat Whiskey (Jar 8/H)")
-    scotch_list.append("Barrell Whiskey Private Release  (Jar 9/I)")
-    scotch_list.append("Loonshine whiskey  (Jar 10/J)")
-    scotch_list.append("Tomatin Cu Bocan (Jar 11/K)")
-    scotch_list.append("n/a (Rest Day #1)")
-    scotch_list.append("Glenglassaugh Portsoy (Jar 13/M)")
-    scotch_list.append("Dampfwerk American Single Malt Whiskey (Jar 14/N)")
-    scotch_list.append("Noah's Mill Bourbon Whiskey (Jar 15/O)")
-    scotch_list.append("Wild Turkey Rare Breed barrel proof (Jar 16/P)")
-    scotch_list.append("Alex Murray Highland 10yr 2011 (Jar 17/Q)")
-    scotch_list.append("jura seven wood  (Jar 18/R)")
-    scotch_list.append("Surprise Whiskey!!!!!!!!! Super Blind Taste test! (Jar 19/S)")
-    scotch_list.append("n/a (Rest Day #2)")
-    scotch_list.append("Driftless Glen single barrel rye (Jar 21/U)")
-    scotch_list.append("Glengoyne 12 (Jar 22/V)")
-    scotch_list.append("The balvenie doublewood (Jar 23/W)")
-    scotch_list.append("Shieldaig 12 Rum Cask Finish (Jar 24/X)")
+    scotch_list.append("Lost Woods 80 (Jar 1/A)")
+    scotch_list.append("Benromach 10 (Jar 2/B)")
+    scotch_list.append("Henry McKenna 10 (Jar 3/C)")
+    scotch_list.append("Hibiki Suntory Whisky (Jar 4/D)")
+    scotch_list.append("Puni Italian Malt Whisky (Jar 5/E)")
+    scotch_list.append("Mystery Whisky (Jar 6/F)")
+    scotch_list.append("Rest Day 1")
+    scotch_list.append("Kaigan Japanese Whisky (Jar 8/G)")
+    scotch_list.append("Grangestone 12 (Jar 9/H)")
+    scotch_list.append("Mellow Corn (Jar 10/I)")
+    scotch_list.append("Toki Suntory Whisky (Jar 11/J)")
+    scotch_list.append("First Call Double Oak (Jar 12/K)")
+    scotch_list.append("WhistlePig Piggyback (Jar 13/L)")
+    scotch_list.append("Lost Woods 80 (again) (Jar 14/M)")
+    scotch_list.append("Rest Day 2")
+    scotch_list.append("Rest Day 3")
+    scotch_list.append("Elijah Craig Toasted Barrel (Jar 17/N)")
+    scotch_list.append("Noah's Mill (Jar 18/O)")
+    scotch_list.append("Rest Day 4")
+    scotch_list.append("Rest Day 5")
+    scotch_list.append("Rest Day 6")
+    scotch_list.append("Rest Day 7")
+    scotch_list.append("Bib Tucker Double Char Bourbon (Jar 23/P)")
+    scotch_list.append("Bowmore 12 (Jar 24/Q)")
+    scotch_list.append("Infinity Bottle (If not already consumed)")
 
     # get todays date - US central is 6 hours behind UTC
     today = datetime.datetime.today() - datetime.timedelta(hours=6)
     print("Date: ", today)
 
     # only output when we want the advent calendar to run
-    if (today.year != 2023): # we don't want this running next year
+    if (today.year != 2025): # we don't want this running next year
         return ("ok", 200)
     if (today.month != 12): # start in december
         return ("ok", 200)
